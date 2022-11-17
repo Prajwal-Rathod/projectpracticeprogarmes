2eabdd26ee4b938e1a57d7552506bb79d3f2b135
Prajwal C S <117572354+Prajwal-Rathod@users.noreply.github.com>
2022-11-17 19:37:00 +0530

    practice

---
+++ practice.py
@@ -0,0 +1,37 @@
+
+# declaring a single integer variable
+integervalue=10
+print(integervalue)
+
+# declaring a single integer variable
+floatvalue=10.5
+print(floatvalue)
+# declaring a singel string variable
+stringvalue="heloworld"
+print(stringvalue)
+stringvalue=20
+print(stringvalue)
+# delaring a sigle boolian variable
+boolianvalue=True
+print(boolianvalue)
+# usage of the type function
+print(type(integervalue))
+# declatring multiple variables in single line
+integervalue, floatvalue=10,10.5
+stringvalue,boolianvalue="helloworld",True
+print(integervalue, floatvalue, stringvalue, boolianvalue)
+# using semi colen
+integervalue=10; print(integervalue)
+# using the input function
+inputvalue = input("enter a value: ")
+print(inputvalue)
+inputvalue=input("enter your name,USN,marks:")
+print(inputvalue)
+# read usn name and marks of the student
+studentname=input("enter student name :")
+studentusn=input("enter student USN:")
+studentmarks=input("enetr student marks")
+print(studentname)
+print(studentusn)
+print(studentmarks)
+

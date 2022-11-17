6181f633139e298a8a0cded7c0cc4133c3a239f6
Prajwal C S <117572354+Prajwal-Rathod@users.noreply.github.com>
2022-11-17 19:28:48 +0530

    dynamic formate

---
+++ days.py
@@ -0,0 +1,15 @@
+# program to calcute secons in day
+seconds_day=24*60*60
+print(seconds_day)
+# program to calcute secons week
+
+seconds_week=7*seconds_day
+print(seconds_week)
+# program to calcute secons in month
+
+seconds_month=30*seconds_week
+print(seconds_month)
+# program to calcute secons in year
+
+seconds_year=12*seconds_month
+print(seconds_year)

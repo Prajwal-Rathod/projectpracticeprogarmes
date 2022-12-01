330aaee9b5fa7514c87f12ebd7ff709c555d9e2c
Prajwal C S <117572354+Prajwal-Rathod@users.noreply.github.com>
2022-12-01 21:12:36 +0530

    take input from the pilot and suggest to land

---
+++ pilot.py
@@ -0,0 +1,12 @@
+# to and the plane whwn it is at an altitude of 5k
+#taking input from the pilot
+print("hello i am your pilot")
+print("can u please inform me on what altitude u are ")
+print("please enter the altitude \n")
+altitude = int(input())
+if (altitude >= 10000 ):
+    print("copy that, please go around ")
+elif (altitude <= 5000 ):
+    print("copy that, can u please land the plane ")
+else:
+    print("invalid input")
